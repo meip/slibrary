ed6ea497e022b20413ee1dec53249c0d97f55f3d
Dominik Freier <freier.dominik@gmail.com>
2013-10-10 16:16:05 +0200

    add TabGUIComponent

---
+++ gui/src/main/java/ch/hsr/slibrary/gui/form/TabGUIComponent.java
@@ -0,0 +1,19 @@
+package ch.hsr.slibrary.gui.form;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dominik
+ * Date: 10.10.13
+ * Time: 16:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class TabGUIComponent extends GUIComponent{
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+
+    public TabGUIComponent() {
+
+    }
+}
