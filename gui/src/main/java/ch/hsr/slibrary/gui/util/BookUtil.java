dd774bf46d45b0dc58900b93f2502a58be8969a1
Dominik Freier <freier.dominik@gmail.com>
2013-10-17 20:54:24 +0200

    improved shortBookName

--- gui/src/main/java/ch/hsr/slibrary/gui/util/BookUtil.java
+++ gui/src/main/java/ch/hsr/slibrary/gui/util/BookUtil.java
@@ -8,10 +8,16 @@ public class BookUtil {
 
     public static String shortBookName(String bookName) {
         int nWords = 5;
+        int maxLength = 25;
         StringBuilder builder = new StringBuilder();
         String[] s = bookName.split(" ");
         for (int i = 0; i < s.length && i < nWords; i++) {
-            boolean isLastSubstring = (i == s.length - 1 || i == nWords - 1);
+            if(builder.length() + s[i].length() > maxLength) {
+                builder.append("...");
+                break;
+            }
+
+            boolean isLastSubstring = (i == s.length - 1 || i == nWords - 1 );
             builder.append(s[i] + ((isLastSubstring) ? "" : " "));
         }
         return builder.toString();
