1b2c84c7045232baf7d2ed8ac14c98b5a2ab3b1e
Philipp Meier <p1meier@hsr.ch>
2013-10-17 14:20:52 +0200

    added BookDetail view

---
+++ gui/src/main/java/ch/hsr/slibrary/gui/controller/BookDetailController.java
@@ -0,0 +1,33 @@
+package ch.hsr.slibrary.gui.controller;
+
+import ch.hsr.slibrary.gui.form.BookDetail;
+import ch.hsr.slibrary.spa.Book;
+import ch.hsr.slibrary.spa.Library;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * User: p1meier
+ * Date: 10.10.13
+ */
+public class BookDetailController extends ComponentController implements Observer {
+
+    private BookDetail bookDetail;
+    private Book book;
+    private Library library;
+
+    public BookDetailController(String title, BookDetail component, Book book, Library library) {
+        super(title);
+        this.component = component;
+        this.bookDetail = component;
+        this.book = book;
+        this.library = library;
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
---
+++ gui/src/main/java/ch/hsr/slibrary/gui/form/BookDetail.java
@@ -0,0 +1,104 @@
+package ch.hsr.slibrary.gui.form;
+
+import javax.swing.*;
+
+/**
+ * User: p1meier
+ * Date: 17.10.13
+ */
+public class BookDetail extends GUIComponent {
+    private JTextField isbnField;
+    private JTextField authorField;
+    private JTextField publisherField;
+    private JTextField titleField;
+    private JComboBox shelfComboBox;
+    private JButton addCopyButton;
+    private JList copyList;
+    private JLabel copyAmountLabel;
+    private JButton removeCopyButton;
+    private JPanel bookDetailPanel;
+
+    public BookDetail() {
+        this.container = bookDetailPanel;
+    }
+
+    public JTextField getIsbnField() {
+        return isbnField;
+    }
+
+    public void setIsbnField(JTextField isbnField) {
+        this.isbnField = isbnField;
+    }
+
+    public JTextField getAuthorField() {
+        return authorField;
+    }
+
+    public void setAuthorField(JTextField authorField) {
+        this.authorField = authorField;
+    }
+
+    public JTextField getPublisherField() {
+        return publisherField;
+    }
+
+    public void setPublisherField(JTextField publisherField) {
+        this.publisherField = publisherField;
+    }
+
+    public JTextField getTitleField() {
+        return titleField;
+    }
+
+    public void setTitleField(JTextField titleField) {
+        this.titleField = titleField;
+    }
+
+    public JComboBox getShelfComboBox() {
+        return shelfComboBox;
+    }
+
+    public void setShelfComboBox(JComboBox shelfComboBox) {
+        this.shelfComboBox = shelfComboBox;
+    }
+
+    public JButton getAddCopyButton() {
+        return addCopyButton;
+    }
+
+    public void setAddCopyButton(JButton addCopyButton) {
+        this.addCopyButton = addCopyButton;
+    }
+
+    public JList getCopyList() {
+        return copyList;
+    }
+
+    public void setCopyList(JList copyList) {
+        this.copyList = copyList;
+    }
+
+    public JLabel getCopyAmountLabel() {
+        return copyAmountLabel;
+    }
+
+    public void setCopyAmountLabel(JLabel copyAmountLabel) {
+        this.copyAmountLabel = copyAmountLabel;
+    }
+
+    public JButton getRemoveCopyButton() {
+        return removeCopyButton;
+    }
+
+    public void setRemoveCopyButton(JButton removeCopyButton) {
+        this.removeCopyButton = removeCopyButton;
+    }
+
+    public JPanel getBookDetailPanel() {
+        return bookDetailPanel;
+    }
+
+    public void setBookDetailPanel(JPanel bookDetailPanel) {
+        this.bookDetailPanel = bookDetailPanel;
+    }
+}
