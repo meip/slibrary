0c1f5f9c59ecccfb78dcf2b567b9d63caa3e956e
Dominik Freier <freier.dominik@gmail.com>
2013-10-17 16:57:52 +0200

    init BookDetail UI

--- gui/src/main/java/ch/hsr/slibrary/gui/controller/BookDetailController.java
+++ gui/src/main/java/ch/hsr/slibrary/gui/controller/BookDetailController.java
@@ -3,7 +3,10 @@ package ch.hsr.slibrary.gui.controller;
 import ch.hsr.slibrary.gui.form.BookDetail;
 import ch.hsr.slibrary.spa.Book;
 import ch.hsr.slibrary.spa.Library;
+import ch.hsr.slibrary.spa.Shelf;
 
+import javax.swing.*;
+import javax.swing.event.ListDataListener;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -23,7 +26,51 @@ public class BookDetailController extends ComponentController implements Observe
         this.bookDetail = component;
         this.book = book;
         this.library = library;
+        this.setTitle(title);
 
+        initializeUI();
+        updateUI();
+
+    }
+
+    private void initializeUI() {
+        bookDetail.getShelfComboBox().setModel(new ComboBoxModel() {
+            @Override
+            public void setSelectedItem(Object anItem) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public Object getSelectedItem() {
+                return null;
+            }
+
+            @Override
+            public int getSize() {
+                return Shelf.values().length;
+            }
+
+            @Override
+            public Object getElementAt(int index) {
+                return Shelf.values()[index];
+            }
+
+            @Override
+            public void addListDataListener(ListDataListener l) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void removeListDataListener(ListDataListener l) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+    }
+
+    public void updateUI() {
+        bookDetail.getTitleField().setText(book.getName());
+        bookDetail.getAuthorField().setText(book.getAuthor());
+        bookDetail.getPublisherField().setText(book.getPublisher());
 
     }
 
