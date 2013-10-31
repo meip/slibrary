4487434b9342b3d8c9cf7aa5d860a05a148d3207
Philipp Meier <p1meier@hsr.ch>
2013-10-31 15:07:35 +0100

    Add: - LoadDetail Form

---
+++ gui/src/main/java/ch/hsr/slibrary/gui/form/LoanDetail.java
@@ -0,0 +1,14 @@
+package ch.hsr.slibrary.gui.form;
+
+import javax.swing.*;
+
+public class LoanDetail {
+    private JTextField customerIdentityField;
+    private JComboBox customerSelect;
+    private JTextField returnDateField;
+    private JLabel amountLabel;
+    private JTable exemplarTable;
+    private JTextField exemplarIdField;
+    private JButton addLoanButton;
+    private JLabel exemplarIdValidationLabel;
+}
