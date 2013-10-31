f54c20cc79362b353dd6272b99e90ef8e1cd6bc9
Philipp Meier <p1meier@hsr.ch>
2013-10-31 15:50:07 +0100

    Add: LoanMaster View

---
+++ gui/src/main/java/ch/hsr/slibrary/gui/form/LoanMaster.java
@@ -0,0 +1,91 @@
+package ch.hsr.slibrary.gui.form;
+
+import javax.swing.*;
+
+public class LoanMaster extends GUIComponent {
+    private JPanel loanMasterPanel;
+    private JLabel loanCountLabel;
+    private JLabel currentLoanLabel;
+    private JLabel overdueLoanLabel;
+    private JTable loanTable;
+    private JTextField searchField;
+    private JCheckBox onlyOverdueCheckbox;
+    private JButton showSelctionButton;
+    private JButton newLoanButton;
+
+    public LoanMaster() {
+        this.container = loanMasterPanel;
+    }
+
+    public JPanel getLoanMasterPanel() {
+        return loanMasterPanel;
+    }
+
+    public void setLoanMasterPanel(JPanel loanMasterPanel) {
+        this.loanMasterPanel = loanMasterPanel;
+    }
+
+    public JLabel getLoanCountLabel() {
+        return loanCountLabel;
+    }
+
+    public void setLoanCountLabel(JLabel loanCountLabel) {
+        this.loanCountLabel = loanCountLabel;
+    }
+
+    public JLabel getCurrentLoanLabel() {
+        return currentLoanLabel;
+    }
+
+    public void setCurrentLoanLabel(JLabel currentLoanLabel) {
+        this.currentLoanLabel = currentLoanLabel;
+    }
+
+    public JLabel getOverdueLoanLabel() {
+        return overdueLoanLabel;
+    }
+
+    public void setOverdueLoanLabel(JLabel overdueLoanLabel) {
+        this.overdueLoanLabel = overdueLoanLabel;
+    }
+
+    public JTable getLoanTable() {
+        return loanTable;
+    }
+
+    public void setLoanTable(JTable loanTable) {
+        this.loanTable = loanTable;
+    }
+
+    public JTextField getSearchField() {
+        return searchField;
+    }
+
+    public void setSearchField(JTextField searchField) {
+        this.searchField = searchField;
+    }
+
+    public JCheckBox getOnlyOverdueCheckbox() {
+        return onlyOverdueCheckbox;
+    }
+
+    public void setOnlyOverdueCheckbox(JCheckBox onlyOverdueCheckbox) {
+        this.onlyOverdueCheckbox = onlyOverdueCheckbox;
+    }
+
+    public JButton getShowSelctionButton() {
+        return showSelctionButton;
+    }
+
+    public void setShowSelctionButton(JButton showSelctionButton) {
+        this.showSelctionButton = showSelctionButton;
+    }
+
+    public JButton getNewLoanButton() {
+        return newLoanButton;
+    }
+
+    public void setNewLoanButton(JButton newLoanButton) {
+        this.newLoanButton = newLoanButton;
+    }
+}
